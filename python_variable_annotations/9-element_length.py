8f1f1a594ae08c959b0aee2107b474f3e1362fa9
sheeny84 <roisin.soulsby@gmail.com>
2025-11-21 10:29:38 +1100

    Split long line for task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) \
+ -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """ Return element length """
     return [(i, len(i)) for i in lst]
