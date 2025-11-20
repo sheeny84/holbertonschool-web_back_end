c8d85422f47c285abc4301d9d4eaf1e655b494c6
sheeny84 <roisin.soulsby@gmail.com>
2025-11-20 16:32:45 +1100

    Ass tasks 2 and 3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This is the floor module """
+import math
+
+
+def floor(n: float) -> int:
+    """ Return the floor of n """
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This is the to_str module """
+
+
+def to_str(n: float) -> str:
+    """ Convert n to a string """
+    return str(n)
