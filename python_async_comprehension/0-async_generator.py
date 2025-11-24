5cc5e4a2223577a48f1e07eec71dc1393599c548
sheeny84 <roisin.soulsby@gmail.com>
2025-11-24 14:57:15 +1100

    Add None as send type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() ->AsyncGenerator[float]:
+async def async_generator() -> AsyncGenerator[float, None]:
     ''' Wait for 1 second and return a number between 0 and 10 '''
     for i in range(10):
         await asyncio.sleep(1)
