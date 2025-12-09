38ee73a5421aa442b877f7cecd6d36f90d15e153
sheeny84 <roisin.soulsby@gmail.com>
2025-12-09 13:47:53 +1100

    Add func doc string for task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -46,6 +46,7 @@ class Server:
         return (start_ind, end_ind)
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """ Return dict of page data """
         length = len(self.dataset())
         total_pages = math.ceil(length/page_size)
         data = self.get_page(page, page_size)
