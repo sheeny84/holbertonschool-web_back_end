45e0edd7f7cd12ff24518054c5055ea7839c2f5e
sheeny84 <roisin.soulsby@gmail.com>
2025-12-10 16:22:44 +1100

    Add task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This is the 'update_topics' module """
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update topics in collection"""
+    filter_query = {"name": name}
+    update_operation = {"$set": {"topics": topics}}
+    mongo_collection.update_one(filter_query, update_operation)
