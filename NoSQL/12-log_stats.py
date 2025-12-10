06770c861927c1ee5caae0b8f441ed2edae35589
sheeny84 <roisin.soulsby@gmail.com>
2025-12-10 17:00:53 +1100

    Add function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,15 +3,20 @@
 import pymongo
 
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client.logs
-collection = db.nginx
-print(f"{collection.count_documents({})} logs")
-print("Methods:")
-print(f"    method GET: {collection.count_documents({'method':'GET'})}")
-print(f"    method POST: {collection.count_documents({'method':'POST'})}")
-print(f"    method PUT: {collection.count_documents({'method':'PUT'})}")
-print(f"    method PATCH: {collection.count_documents({'method':'PATCH'})}")
-print(f"    method DELETE: {collection.count_documents({'method':'DELETE'})}")
-print(f"""{collection.count_documents({'method': 'GET',
-                                     'path': '/status'})} status check""")
+def log_stats():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+    print(f"""{collection.count_documents({'method': 'GET',
+                                        'path': '/status'})} status check""")
+
+
+if __name__ == "__main__":
+    log_stats()
