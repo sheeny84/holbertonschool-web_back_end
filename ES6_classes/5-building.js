1d8fcf8d1565d4e705cf039089e8c5ef460c02c0
sheeny84 <roisin.soulsby@gmail.com>
2026-01-06 18:10:29 +1100

    Add comments to task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    // Check if this in a subclass instance
     if (this.constructor !== Building) {
+      // Check if evacuationWarningMessage has been instantiated
+      // i.e. if it is the same as Building then it has not
       if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+        // Call the default warning message
         this.evacuationWarningMessage();
       }
     }
