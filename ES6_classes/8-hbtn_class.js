26e87fc3efdfdbc9b9392eab1b84a607a65fe112
sheeny84 <roisin.soulsby@gmail.com>
2026-01-07 10:19:47 +1100

    Add task 9 and add comments to task 8

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -4,10 +4,12 @@ export default class HolbertonClass {
     this._location = location;
   }
 
+  // When casting to a string, js calls toString internally
   toString() {
     return this._location;
   }
 
+  // When casting to a number, js calls valueOf internally
   valueOf() {
     return this._size;
   }
