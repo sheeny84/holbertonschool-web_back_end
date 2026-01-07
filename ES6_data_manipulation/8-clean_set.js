cf9b758d491df63dee180569a4a7157fe044c3e6
sheeny84 <roisin.soulsby@gmail.com>
2026-01-07 17:57:03 +1100

    Add task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (startString.length === 0) {
+    return result;
+  }
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      const ending = (item.slice(startString.length));
+      if (result.length !== 0) {
+        result += `-${ending}`;
+      } else {
+        result += ending;
+      }
+    }
+  }
+  return result;
+}
