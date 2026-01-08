4ed7077b12bbf46bd6148abe89e10d463a60c310
sheeny84 <roisin.soulsby@gmail.com>
2026-01-08 14:37:16 +1100

    Fix success typo in task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({
+  return promise
+  .then(() => ({
       status: 200,
-      body: 'Success',
-    })
-  ).catch(() => new Error()
+      body: 'success',
+  })).catch(() => new Error()
   ).finally(() => {
     console.log('Got a response from the API');
   });
