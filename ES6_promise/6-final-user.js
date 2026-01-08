dc7d336baa642f3d8365055f237c52c1a9c1d6e5
sheeny84 <roisin.soulsby@gmail.com>
2026-01-08 15:43:12 +1100

    Update task 6 to use allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,19 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-  return Promise.all([promise1, promise2])
-    .then((user, photo) => ([
-      {
-        status: 'fulfilled',
-        value: user,
-      }
-    ]))
-    .catch((error) => ([
-      { 
-        status: 'rejected',
-        value: error.toString(),
-      }
-    ]));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then(results =>
+      results.map(result => {
+        if (result.status === 'fulfilled') {
+          return { status: result.status, value: result.value };
+        } else {
+          return { status: result.status, value: result.reason.toString() };
+        }
+      })
+    );
 }
