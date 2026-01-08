f9ea6a2b904a5789ce5abe8e3ee7f97c3006fb7a
sheeny84 <roisin.soulsby@gmail.com>
2026-01-08 14:59:19 +1100

    Add return to task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 // createUser returns firstName, lastName
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
