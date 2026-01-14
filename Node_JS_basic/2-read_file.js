30a6db93b2fe253b59374bdfea7834d95728c6ca
sheeny84 <roisin.soulsby@gmail.com>
2026-01-15 10:18:41 +1100

    Add task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let data;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  // get rows of data
+  let rows = data.trim().split('\n');
+  // remove first line which is the headings for each col
+  rows = rows.slice(1);
+
+  // students is an array of objects with key-value pairs for firstname and field
+  const students = rows.map((row) => {
+    // split each row of the array into its values
+    const [firstname, , , field] = row.split(',');
+    // return an object with firstname and field key-value pairs
+    return { firstname, field };
+  });
+  console.log(`Number of students: ${students.length}`);
+
+  // create an empty object for the processed student data
+  const processedStudents = {};
+  students.forEach(({ firstname, field }) => {
+    // if processedStudents doesn't already contain the field, add it
+    if (!processedStudents[field]) {
+      // add the field and set the value to an empty array
+      processedStudents[field] = [];
+    }
+    // add the firstname to names array for its field
+    processedStudents[field].push(firstname);
+  });
+
+  // processedStudents has key-value pairs for each field where value is the array of names
+  for (const field in processedStudents) {
+    if (processedStudents[field]) {
+      const names = processedStudents[field];
+      console.log(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`,
+      );
+    }
+  }
+}
+
+module.exports = countStudents;
