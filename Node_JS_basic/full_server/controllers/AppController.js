9fc7a4f2b7dd53d101a23d6b7cfd0507f81e3b42
sheeny84 <roisin.soulsby@gmail.com>
2026-01-15 15:56:32 +1100

    Fix app controller response syntax

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,6 @@
 class AppController {
   static getHomepage(request, response) {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello Holberton School!');
+    response.status(200).send('Hello Holberton School!');
     return response;
   }
 }
